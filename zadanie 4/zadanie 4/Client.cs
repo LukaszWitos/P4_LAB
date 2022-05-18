98cdde7663aca00b38e24213002188e8528d93c3
LukaszWitos <100522783+LukaszWitos@users.noreply.github.com>
2022-05-18 16:28:01 +0200

    zatwierdzono

---
+++ zadanie 4/zadanie 4/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    internal class Client
+    {
+        public Client()
+        {
+            Orders = new List<Order>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
--- zadanie 4/zadanie 4/Program.cs
+++ zadanie 4/zadanie 4/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using lab5;
+using Microsoft.EntityFrameworkCore;
+
 Console.WriteLine("Hello, World!");
+
+var context = new MyDbContext();
+context.Database.EnsureCreated();
+/*var client =
+    new Client()
+    {
+        Name = "Jan Kowalski"
+    };
+
+client.Orders.Add(new Order()
+{
+    Price = 10m
+});
+ context.Clients.Add(client);
+
+context.SaveChanges();
+*/
+foreach (var item in context.Clients
+    .Include(x => x.Orders)
+    .ToList())
+{
+    Console.WriteLine(item.Name);
+}
