3d4d2818c1686177d8820e26a16721cae06f8627
LukaszWitos <100522783+LukaszWitos@users.noreply.github.com>
2022-05-19 19:35:20 +0200

    zatwierdzono

---
+++ zadanie 4/zadanie 4/Ksiazka.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_4
+{
+    internal class Ksiazka
+    {
+        public int Id { get; set; }
+        public string Tytul { get; set; }
+
+        public int rok { get; set; }
+        [ForeignKey("Autor")]
+        public string Autor { get; set; }
+        public Autor autor { get; set; }
+    }
+}
--- zadanie 4/zadanie 4/MyDbContext.cs
+++ zadanie 4/zadanie 4/MyDbContext.cs
@@ -4,27 +4,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using zadanie_4;
 
 namespace lab5
 {
     internal class MyDbContext : DbContext
     {
-        public DbSet<Client> Clients { set; get; }
-        public DbSet<Order> Orders { set; get; }
+        public DbSet<Autor> Autor { set; get; }
+        public DbSet<Ksiazka> Ksiazka { set; get; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PIV2-Relations;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Zadanie;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
-        ////protected override void OnModelCreating(ModelBuilder modelbuilder)
-        ////{
-        ////    base.OnModelCreating(modelbuilder);
-        ////    modelbuilder.Entity<Client>().Property("Id")
-        ////    .IsRequired()
-        ////    .HasMaxLength(50);
-        ////    modelbuilder.Entity<Order>
-        ////}
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+
+
+
+
+            base.OnModelCreating(modelbuilder);
+            modelbuilder.Entity<Autor>().Property("Nazwisko");
+
+            //modelbuilder.Entity<Ksiazka>;
+        }
 
     }
 }
