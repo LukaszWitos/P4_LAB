d32a0020c9e74d384f74623bfc4aa9841827e47e
LukaszWitos <100522783+LukaszWitos@users.noreply.github.com>
2022-07-10 19:20:56 +0200

    adsda

--- Zadanie6/App.xaml.cs
+++ Zadanie6/App.xaml.cs
@@ -6,12 +6,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Zadanie6
+namespace lab8
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+
     }
 }
--- Zadanie6/MainWindow.xaml.cs
+++ Zadanie6/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,13 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Zadanie6
+namespace lab8
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,6 +25,22 @@ namespace Zadanie6
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Resources["myDrush"] = new SolidColorBrush(Colors.Aquamarine);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var path = Environment.CurrentDirectory + @"\Dictionary1.xaml";
+
+            using var stream = new FileStream(path, FileMode.Open);
+
+            var content = XamlReader.Load(stream);
+
+            this.Resources.MergedDictionaries.Add((ResourceDictionary)content);
         }
     }
 }
